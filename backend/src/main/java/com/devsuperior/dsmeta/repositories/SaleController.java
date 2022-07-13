d2445099608f5cfa1807b373a194c9c005e5d3e8
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2022-07-13 20:10:40 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleController.java
@@ -3,14 +3,17 @@ package com.devsuperior.dsmeta.repositories;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.entities.services.SaleService;
+import com.devsuperior.dsmeta.entities.services.SmsService;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -19,6 +22,9 @@ public class SaleController {
 	@Autowired
 	private SaleService service;
 	
+	@Autowired
+	private SmsService smsService;
+	
 	@GetMapping
 	public ResponseEntity<Page<Sale>> findSales(
 			@RequestParam(value = "minDate", defaultValue = "") String minDate,
@@ -26,5 +32,11 @@ public class SaleController {
 			Pageable pageable) {
 		Page<Sale> sale = service.findSales(minDate, maxDate, pageable);
 		return ResponseEntity.ok(sale);
+	}
+	
+	@GetMapping(value = "/{id}/notification")
+	public ResponseEntity<Void> notifiySms(@PathVariable("id") Long id) {
+		smsService.sendSms(id);
+		return ResponseEntity.status(HttpStatus.OK).build();
 	}
 }
