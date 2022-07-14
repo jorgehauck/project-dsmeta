17e3cc8f20b1ff1c0daa6e0a9947a4654af217b5
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2022-07-13 21:12:45 -0300

    Alteração de arquivos

--- backend/src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dsmeta.repositories;
+package com.devsuperior.dsmeta.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
--- backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -9,13 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 @Entity
 @Table(name = "tb_sales")
-@NoArgsConstructor
-@Data
 public class Sale implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
@@ -28,4 +23,64 @@ public class Sale implements Serializable {
 	private Double amount;
 	private LocalDate date;
 	
+	public Sale() {
+	}
+
+	public Sale(Long id, String sellerName, Integer visited, Integer deals, Double amount, LocalDate date) {
+		super();
+		this.id = id;
+		this.sellerName = sellerName;
+		this.visited = visited;
+		this.deals = deals;
+		this.amount = amount;
+		this.date = date;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSellerName() {
+		return sellerName;
+	}
+
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+
+	public Integer getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
 }
